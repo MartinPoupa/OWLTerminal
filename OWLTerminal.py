ac700c70010717f78a6dd1a32c6884e649f1b44f
MartinPoupa <58166333+MartinPoupa@users.noreply.github.com>
2020-11-12 11:58:47 +0100

    Update OWLTerminal.py

--- OWLTerminal.py
+++ OWLTerminal.py
@@ -5,6 +5,30 @@ import time
 import getpass
 import sys
 from termcolor import colored
+import glob
+
+
+def serial_ports():
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
 
 def serialRead(Serial):
     global pause
@@ -15,9 +39,10 @@ def serialRead(Serial):
                 now = datetime.now()
                 curentTime = now.strftime("%H:%M:%S.%f")
                 serialMessage = bytestoRead.decode("utf-8").replace("\n","").replace("\r","")
-                print("\n" + curentTime +  colored(" -->", 'green') + " : " + serialMessage, end = "")
+                print(curentTime +  colored(" -->", 'green') + " : " + serialMessage)
+                # print(curentTime + " --> : " + serialMessage)
         except Exception as e:
-            print("\n" + str(e)) 
+            print(str(e)) 
             exit()       
                 
 def serialWrite(Serial):
@@ -29,14 +54,34 @@ def serialWrite(Serial):
                 now = datetime.now()
                 curentTime = now.strftime("%H:%M:%S.%f")
                 Serial.write((buffer + "\n").encode("UTF-8"))
-                
-                print(curentTime + colored(" <--", 'red') + " : " + buffer, end = "")
+                sys.stdout.write("\033[F")
+                print(curentTime + colored(" <--", 'red') + " : " + buffer)
+                # print(curentTime + " <-- : " + buffer)
         except Exception as e:
-            print("\n" + str(e)) 
+            print(str(e)) 
             exit()
+port = ""
+baud = 0
+ports = serial_ports()
+try:
+    port = sys.argv[1]
+    baud = int(sys.argv[2])
+except:
+    pass
+    
+if(port == ""):
+    for i in range(len(ports)):
+        print(str(i) + " : " + str(ports[i]))
+    print(str(len(ports)) + " : different port")
+    numberPort = int(input("type port number: "))
+    if len(ports) > numberPort:
+        port = ports[numberPort].replace("tty.", "cu.")
+    else:
+        port = input("type port name: ")
 
-port = sys.argv[1]
-baud = int(sys.argv[2])
+if(baud == 0):
+    baud = int(input("baudrate: "))
+
 
 Serial = serial.Serial()
 Serial.port = port
